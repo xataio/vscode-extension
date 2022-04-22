de350c7d552a3b92e0ac3be67deeab5308989ea1
Fabien BERNARD <fabien0102@hotmail.com>
2022-04-22 23:28:02 +0200

    Add "add workspace" command

---
+++ src/commands/addWorkspace.ts
@@ -0,0 +1,36 @@
+import * as vscode from "vscode";
+import { Command } from "../types";
+import { createWorkspace } from "../xata/xataComponents";
+
+export const addWorkspaceCommand: Command = {
+  id: "xata.addWorkspace",
+  type: "global",
+  icon: "add",
+  action(context, explorer) {
+    return async () => {
+      const name = await vscode.window.showInputBox({
+        prompt: "Enter the name of your workspace",
+        title: "Workspace name",
+      });
+
+      if (!name) {
+        return;
+      }
+
+      await createWorkspace({
+        baseUrl: context.getBaseUrl(),
+        context,
+        body: {
+          name,
+          slug: slugify(name),
+        },
+      });
+
+      return explorer.refresh();
+    };
+  },
+};
+
+function slugify(name: string) {
+  return name.toLowerCase().split(/\W/g).filter(Boolean).join("-");
+}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,7 @@
 export { loginCommand } from "./login";
 export { logoutCommand } from "./logout";
 export { refreshCommand } from "./refresh";
+export { addWorkspaceCommand } from "./addWorkspace";
 
 // TreeItem
 export { addColumnCommand } from "./addColumn";
