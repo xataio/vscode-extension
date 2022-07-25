bb5a40fb2f8846cf9391c70d1e98d8ee553ad22a
Fabien BERNARD <fabien0102@hotmail.com>
2022-07-25 11:46:48 +0200

    Handle non git env

--- src/commands/createBranch.ts
+++ src/commands/createBranch.ts
@@ -3,11 +3,14 @@ import { VSCodeWorkspaceTreeItem } from "../views/treeItems/TreeItem";
 import { TreeItemCommand, WorkspaceNavigationItem } from "../types";
 import { createBranch, getBranchList } from "../xata/xataComponents";
 import { ValidationError } from "../xata/xataFetcher";
+import { validateResourceName } from "../utils";
 
 /**
  * Create a xata branch based on the actual git branch.
  *
  * If the xata branch and the git branch are already in sync, ask for creating a new branch.
+ *
+ * If the workspace doesn't have any git environment, the branching will be handled with the `XATA_BRANCH` env var instead.
  */
 export const createBranchCommand: TreeItemCommand<
   VSCodeWorkspaceTreeItem | WorkspaceNavigationItem
@@ -33,7 +36,7 @@ export const createBranchCommand: TreeItemCommand<
 
       const currentGitBranch = await context.getGitBranch(workspaceFolder.uri);
 
-      if (!config || !currentGitBranch) {
+      if (!config) {
         return;
       }
 
@@ -56,7 +59,25 @@ export const createBranchCommand: TreeItemCommand<
 
       let dbBranchName: string;
 
-      if (config.branch !== currentGitBranch) {
+      if (!currentGitBranch) {
+        const newBranchName = await vscode.window.showInputBox({
+          prompt: "Enter the name of your branch",
+          title: "Branch name",
+          validateInput: validateResourceName("branch", existingBranches),
+        });
+
+        if (!newBranchName) {
+          return;
+        }
+        await updateXataBranchInDotEnv(workspaceFolder, newBranchName);
+        dbBranchName = `${config.databaseName}:${newBranchName}`;
+        from =
+          existingBranches.length === 1
+            ? existingBranches[0]
+            : await vscode.window.showQuickPick(existingBranches, {
+                title: "Base branch",
+              });
+      } else if (config.branch !== currentGitBranch) {
         dbBranchName = `${config.databaseName}:${currentGitBranch}`;
         from =
           existingBranches.length === 1
@@ -102,3 +123,31 @@ export const createBranchCommand: TreeItemCommand<
     };
   },
 };
+
+async function updateXataBranchInDotEnv(
+  workspaceFolder: vscode.WorkspaceFolder,
+  branchName: string
+) {
+  const envPath =
+    vscode.workspace.getConfiguration().get<string>("xata.envFilePath") ??
+    ".env";
+
+  const envFileRaw = await await vscode.workspace.fs.readFile(
+    vscode.Uri.joinPath(workspaceFolder.uri, envPath)
+  );
+
+  let envFile = envFileRaw.toString();
+  if (envFile.match(/^XATA_BRANCH=/gm)) {
+    envFile = envFile.replace(
+      /^(XATA_BRANCH=)([a-zA-Z0-9_\-~:]+)/gm,
+      `$1${branchName}`
+    );
+  } else {
+    envFile += `${envFile.length ? "\n" : ""}XATA_BRANCH=${branchName}`;
+  }
+
+  await vscode.workspace.fs.writeFile(
+    vscode.Uri.joinPath(workspaceFolder.uri, envPath),
+    Buffer.from(envFile)
+  );
+}
