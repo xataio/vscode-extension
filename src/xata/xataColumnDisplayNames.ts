f54c19dd9112642f9b1579d0f551f16e83a36883
Fabien BERNARD <fabien0102@hotmail.com>
2022-08-24 15:51:29 +0200

    fix: use display name for column types
    Closed Align column type names with the Web UI #57

---
+++ src/xata/xataColumnDisplayNames.ts
@@ -0,0 +1,14 @@
+import { Column } from "./xataSchemas";
+
+export const xataColumnDisplayNames: Record<Column["type"], string> = {
+  bool: "Boolean",
+  datetime: "Date",
+  email: "Email",
+  float: "Decimal number",
+  int: "Integer number",
+  multiple: "Multiple select",
+  string: "String",
+  text: "Long text",
+  link: "Link",
+  object: "Object",
+};
